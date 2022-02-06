edfa40a052ece53de43f1ef6382204d8a5c3483c
Tiffany <tiff.sobczak@gmail.com>
2022-02-06 14:55:38 -0600

    added department questions

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const mysql = require('mysql2/promise');
+const inquirer= require ('inquirer');
 
+const questions =[
+    {
+    type: 'list',
+    name: 'action',
+    message: 'Choose an action:',
+    choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+    }]
 
+const required = (attr, input) => {
+    if (input) {
+        return true;
+        //when there is no input
+    } else {
+        console.log(`Please enter valid input for ${attr}`);
+        return false;
+    }
+}
 
 const main = async() => {
     const connection = await mysql.createConnection({
@@ -10,10 +27,34 @@ const main = async() => {
         password: '',
         database: 'Employee Tracker'
     });
-    const [rows] = await connection.query (
-        'SELECT * FROM role'
-    )
-    console.table (rows)
+    const answers=await inquirer.prompt (questions)
+    if (answers.action === 'view all departments') {
+        const [departmentRows] = await connection.query ('SELECT * FROM department') 
+        console.table (departmentRows)
+    } else if (answers.action === 'view all roles') {
+        const [rolesRows] = await connection.query ('SELECT * FROM role') 
+        console.table (rolesRows)
+    } else if (answers.action === 'view all employees') {
+        const [employeeRows] = await connection.query ('SELECT * FROM employee') 
+        console.table (employeeRows)
+    } else if (answers.action === 'add a department') {
+        const deptAnswers=await inquirer.prompt ([{
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the department?',
+            validate: input => required("name", input)
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of the department?',
+            validate: input => required("description", input)
+        }])
+        
+    }
+    
+    process.exit(0)
 }
+
+
 
 main (); 
